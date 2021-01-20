2c048830a0f59617542d613c3a1e56d11a4bb614
Caitlin Crews <caitlinrcrews@gmail.com>
2021-01-20 00:38:00 -0500

    set up initial router

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const db = require("../models/");
+
+router.get("/api/workouts", (req,res) => {
+  db.Workout.find({})
+    .then((data) => {
+      res.json(data)
+    }).catch((err) => res.json(err.message));
+});
+
+module.exports = router;
