ddf98fb7405b9b4caa061d124da141c7e8bb1aed
Caitlin Crews <caitlinrcrews@gmail.com>
2021-01-21 00:39:18 -0500

    added api post route - incomplete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
 const db = require("../models");
 
+// gets all workouts from db api
 router.get("/api/workouts", (req,res) => {
   db.Workout.find({})
     .then((data) => {
       res.json(data)
     }).catch((err) => res.json(err.message));
+});
+
+// posts a new workout
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+  .then((dbWorkout) => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  });
 });
 
 module.exports = router;
