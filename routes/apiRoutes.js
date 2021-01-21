00e3dcf55a97746be2e9e88d82a140fe48430d75
Caitlin Crews <caitlinrcrews@gmail.com>
2021-01-21 18:32:06 -0500

    updated api post and put routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,38 @@
 const router = require("express").Router();
-const db = require("../models");
+const Workout = require("../models/workout.js");
 
 // gets all workouts from db api
 router.get("/api/workouts", (req,res) => {
-  db.Workout.find({})
+  Workout.find({})
     .then((data) => {
       res.json(data)
     }).catch((err) => res.json(err.message));
 });
 
 // posts a new workout
-router.post("/api/workouts", ({ body }, res) => {
-  db.Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+  //console.log(req.body);
+
+  Workout.create({})
   .then((dbWorkout) => {
     res.json(dbWorkout)
+    console.log(dbWorkout)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  });
+});
+
+router.put('/api/workouts/:id', ({body, params}, res) => {
+  console.log(body);
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body }},
+    {new: true, runValidators: true }
+  )
+  .then((dbWorkout) => {
+    res.json(dbWorkout)
+    console.log(dbWorkout)
   })
   .catch(err => {
     res.status(400).json(err)
