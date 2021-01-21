782e0a8c9be81caaf102e3ba2b4e915f10134630
Caitlin Crews <caitlinrcrews@gmail.com>
2021-01-21 00:38:36 -0500

    included all required html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,16 @@
 const path = require("path");
+const router = require("express").Router();
 
-module.exports = function(app) {
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
-};
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
