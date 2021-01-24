68c677f99708246c1593efcbb750cd1f33330cdb
Caitlin Crews <caitlinrcrews@gmail.com>
2021-01-23 22:55:30 -0500

    finishedgit status set up last get request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,6 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 // gets all workouts from db api
-// router.get("/workouts", (req,res) => {
-//   Workout.find({})
-//     .then((data) => {
-//       res.json(data)
-//     }).catch((err) => res.status(400).json(err.message));
-// });
-
 router.get("/workouts", (req,res) => {
   Workout.aggregate([
     {
@@ -39,13 +32,33 @@ router.post("/workouts", (req, res) => {
   });
 });
 
-router.put('/workouts/:id', ({body, params}, res) => {
+router.put("/workouts/:id", ({body, params}, res) => {
   console.log(body);
   Workout.findByIdAndUpdate(
     params.id,
     { $push: { exercises: body }},
     {new: true, runValidators: true }
   )
+  .then((dbWorkout) => {
+    res.json(dbWorkout)
+    console.log(dbWorkout)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  });
+});
+
+router.get("/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        }
+      }
+    }
+  ])
+  .sort({ day: 1 })
   .then((dbWorkout) => {
     res.json(dbWorkout)
     console.log(dbWorkout)
