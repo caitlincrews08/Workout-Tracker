3710cc7e9ffc8baf0396760eba1a7eeb924db0b5
Caitlin Crews <caitlinrcrews@gmail.com>
2021-01-25 00:10:32 -0500

    formatted doc

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 // gets all workouts from db api
-router.get("/workouts", (req,res) => {
+router.get("/workouts", (req, res) => {
   Workout.aggregate([
     {
       $addFields: {
@@ -23,29 +23,29 @@ router.post("/workouts", (req, res) => {
   //console.log(req.body);
 
   Workout.create({})
-  .then((dbWorkout) => {
-    res.json(dbWorkout)
-    console.log(dbWorkout)
-  })
-  .catch(err => {
-    res.status(400).json(err)
-  });
+    .then((dbWorkout) => {
+      res.json(dbWorkout)
+      console.log(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    });
 });
 
-router.put("/workouts/:id", ({body, params}, res) => {
+router.put("/workouts/:id", ({ body, params }, res) => {
   console.log(body);
   Workout.findByIdAndUpdate(
     params.id,
-    { $push: { exercises: body }},
-    {new: true, runValidators: true }
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
   )
-  .then((dbWorkout) => {
-    res.json(dbWorkout)
-    console.log(dbWorkout)
-  })
-  .catch(err => {
-    res.status(400).json(err)
-  });
+    .then((dbWorkout) => {
+      res.json(dbWorkout)
+      console.log(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    });
 });
 
 // gets data for user dashboar
@@ -59,14 +59,14 @@ router.get("/workouts/range", (req, res) => {
       }
     }
   ])
-  .sort({ day: 1 })
-  .then((dbWorkout) => {
-    res.json(dbWorkout)
-    console.log(dbWorkout)
-  })
-  .catch(err => {
-    res.status(400).json(err)
-  });
-}); 
+    .sort({ day: 1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout)
+      console.log(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    });
+});
 
 module.exports = router;
